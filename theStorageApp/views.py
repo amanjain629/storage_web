6a48f74f78007978e49762899794d7c6615122b9
amanj629 <amanj629@gmail.com>
2021-10-19 20:19:10 +0530

    fifth commit

--- theStorageApp/views.py
+++ theStorageApp/views.py
@@ -5,13 +5,13 @@ from django.contrib import auth
 # Create your views here.
 
 config = {
-    "apiKey": "AIzaSyCrk_8dwpcXurAz-oa5QGohs2On8Fk_2xs",
-    "authDomain": "storagesystem-83746.firebaseapp.com",
-    "databaseURL": "https://storagesystem-83746-default-rtdb.firebaseio.com",
-    "projectId": "storagesystem-83746",
-    "storageBucket": "storagesystem-83746.appspot.com",
-    "messagingSenderId": "590468774618",
-    "appId": "1:590468774618:web:22824e72ba86ec170683e3",
+    "apiKey": "AIzaSyCIfGntwga6XKHOLWvFI24k_ZjzW6WFkdk",
+    "authDomain": "arcloud-7e352.firebaseapp.com",
+    "databaseURL": "https://arcloud-7e352-default-rtdb.firebaseio.com",
+    "projectId": "arcloud-7e352",
+    "storageBucket": "arcloud-7e352.appspot.com",
+    "messagingSenderId": "836597662489",
+    "appId": "1:836597662489:web:e6bd2531dc693d9d44bb88",
 }
 
 firebase = pyrebase.initialize_app(config)
@@ -32,6 +32,7 @@ def PostFeedbackView(request):
     name=request.POST.get('name')
     email=request.POST.get('mail')
     feedback=request.POST.get('feedback')
+    print(name,email,feedback)
     data = {"name":name,"email":email,"feedback":feedback}
     database.child('feedback').child('users').push(data)
     return render(request,"index.html")
